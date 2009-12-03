25a741a23d3a040b6949a93653377a7cecd1d0a2
Joni <joni.lahtinen@helsinki.fi>
2009-12-03 21:56:50 +0200

    jotain

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  
   acts_as_authorization_subject
   
   validates_presence_of :name, :password, :unless => :now_editing
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091203100030) do
+ActiveRecord::Schema.define(:version => 20091203122919) do
 
   create_table "course_instances", :force => true do |t|
     t.string   "lecture_time"
@@ -75,7 +75,11 @@ ActiveRecord::Schema.define(:version => 20091203100030) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "student_number"
-    t.string   "email",          :null => false
+    t.string   "email",               :null => false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
